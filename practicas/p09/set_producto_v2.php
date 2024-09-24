6c3c6b331675ebee2ef0be202be07fde5b716350
morales512 <juan.moralesper@alumno.buap.mx>
2024-09-24 14:30:12 -0600

    Se concluyo la quinta y sexta parte exitosamente

--- practicas/p09/set_producto_v2.php
+++ practicas/p09/set_producto_v2.php
@@ -23,16 +23,18 @@ $resultado = $link->query($sql_verificar);
 if ($resultado->num_rows > 0) {
     echo 'Error: El producto con este nombre, marca y modelo ya existe en la base de datos.';
 } else {
-    // Insertar el producto si no existe ya en la base de datos
-    //SE AÑADE LA VERIFICACION DE LA COLUMNA ELIMINADO
-    $sql_insertar = "INSERT INTO productos VALUES (null, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}', '{$eliminado}')";    
+    // Insertar el producto con el valor '0' en la columna 'eliminado'
+    //SE COMENTA
+    //$sql_insertar = "INSERT INTO productos VALUES (null, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}', 0)";
+    $sql_insertar = "INSERT INTO productos (nombre, marca, modelo, precio, detalles, unidades, imagen, eliminado) 
+                    VALUES ('{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}', )";
     if ($link->query($sql_insertar)) {
         echo 'Producto insertado con éxito:<br/>';
         echo 'Nombre: ' . $nombre . '<br/>';
         echo 'Marca: ' . $marca . '<br/>';
         echo 'Modelo: ' . $modelo . '<br/>';
         echo 'Precio: ' . $precio . '<br/>';
-        echo 'Detalles: ' . zdetalles . '<br/>';
+        echo 'Detalles: ' . $detalles . '<br/>';
         echo 'Unidades: ' . $unidades . '<br/>';
         echo 'Imagen: ' . $imagen . '<br/>';
     } else {
