ea6804141b040a1c9a027f9524eedfc4d38f6aec
morales512 <juan.moralesper@alumno.buap.mx>
2024-09-24 13:48:29 -0600

    Se concluyo la TERCERA parte exitosamente

--- practicas/p09/set_producto_v2.php
+++ practicas/p09/set_producto_v2.php
@@ -24,6 +24,7 @@ if ($resultado->num_rows > 0) {
     echo 'Error: El producto con este nombre, marca y modelo ya existe en la base de datos.';
 } else {
     // Insertar el producto si no existe ya en la base de datos
+    //SE AÑADE LA VERIFICACION DE LA COLUMNA ELIMINADO
     $sql_insertar = "INSERT INTO productos VALUES (null, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}', '{$eliminado}')";    
     if ($link->query($sql_insertar)) {
         echo 'Producto insertado con éxito:<br/>';
